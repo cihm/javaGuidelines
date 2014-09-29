89220292a928e9128c81db6638ee799ac24ac8de
Lewis <b9715013@mail.ntust.edu.tw>
2014-09-29 11:26:49 +0800

    Estimate

--- javaGuid.java
+++ javaGuid.java
@@ -6,4 +6,9 @@
 3. finally var is all upper,
     and use  _  to separa
 
-4. String	
+4. String	test;
+   if(test.equals("targer") )     V
+   if("target" .equals(tset) )    O  recommend  
+   
+5. String final SIZE = 1000/300;
+   String final SIZE_2 = 2.67; // Estimate   
