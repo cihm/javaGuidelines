9af85878f075f29bce7494518f069ea2aa269f8a
Lewis <b9715013@mail.ntust.edu.tw>
2014-09-29 11:50:42 +0800

    new

--- javaGuid.java
+++ javaGuid.java
@@ -1,14 +1,15 @@
 1. "method" and "var"
-     first word must be 
-     lower
-2. class  is upper
+     first word must be lower //ex: getUserName
+	 
+2. class  is upper            //ex: GetUserName 
 
-3. finally var is all upper,
-    and use  _  to separa
+3. finally var is all upper, and use _ to separa 
 
 4. String	test;
-   if(test.equals("targer") )     V
-   if("target" .equals(tset) )    O  recommend  
+   if(test.equals("targer") )     X
+   if("target" .equals(tset) )    O  recommend
+   //if test = null , line 1 will nullPointerError,   
+   //Never happened before because declare well
    
 5. String final SIZE = 1000/300;
    String final SIZE_2 = 2.67; // Estimate   
